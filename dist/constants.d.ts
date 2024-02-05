fa52b333975d20eafa35f9953cf5cf1eae06f0c1
DeFi Pete <pete@gordian.agency>
2024-02-05 10:18:39 +0100

    init

---
+++ dist/constants.d.ts
@@ -0,0 +1,13 @@
+import { Pulse, Token } from '@pulsex/sdk-core';
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x955219a87eb0c6754fd247266af970f7d16906cd";
+export declare const WETH: {
+    369: Token;
+};
+export declare const ETHER: Pulse;
+export declare const INIT_CODE_HASH = "0xd98d5db929ea0eea7a1e5d6b95fe5a6bca2f96da961c8c025a31487e3354fd80";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+import { Pulse, Token } from '@pulsex/sdk-core'
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x955219a87eb0c6754fd247266af970f7d16906cd'
+
+export const WETH = { 369: new Token(369, '0xa1077a294dde1b09bb078844df40758a5d0f9a27', 18, 'WPLS', 'Wrapped Pulse') }
+
+export const ETHER = { ...Pulse.onChain(369), wrapped: WETH[369], chainId: 369, equals: Pulse.onChain(369).equals } as Pulse
+
+export const INIT_CODE_HASH = '0xd98d5db929ea0eea7a1e5d6b95fe5a6bca2f96da961c8c025a31487e3354fd80'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const FEES_DENOMINATOR = JSBI.BigInt(10000)
